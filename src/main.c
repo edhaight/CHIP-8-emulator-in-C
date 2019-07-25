3261ff6d4c7dc371f8eedbdb1a189c6e6034b9aa
Erik Haight <edhaight@ucsc.edu>
2019-07-25 14:55:39 -0700

    Refactor fail checks

--- src/main.c
+++ src/main.c
@@ -4,6 +4,10 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
 
+#define WINDOW_WIDTH 64
+#define WINDOW_HEIGHT 32
+#define WINDOW_SCALE 10
+
 struct display
 {
     SDL_Window *window;
@@ -12,43 +16,69 @@ struct display
 };
 typedef struct display display;
 
+// https : //github.com/scanlong/c8/blob/master/src/display.c
+int checkDisplayError(char *component, void *pointer)
+{
+    if (pointer == NULL)
+    {
+        printf("Could not create %s: %s\n", component, SDL_GetError());
+        return -1;
+    }
+    return 0;
+}
+int setupDisplay(display *display)
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) // Try to initialize an SDL video component
+    {
+        printf("Error initializing SDL: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    display->window = SDL_CreateWindow(
+        "Chip 8 Emu", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        WINDOW_WIDTH * WINDOW_SCALE, WINDOW_HEIGHT * WINDOW_SCALE, SDL_WINDOW_SHOWN);
+
+    if (checkDisplayError("window", display->window))
+        return -1;
+
+    display->renderer = SDL_CreateRenderer(display->window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (checkDisplayError("renderer", display->renderer))
+        return -1;
+
+    return 0;
+}
+
+void checkFailure(char *failMsg, char *successMsg, int failed)
+{
+    if (failed)
+    {
+        printf("Failed to %s!\n", failMsg);
+        exit(1);
+    }
+    else
+    {
+        printf("Succesfully %s!\n", successMsg);
+    }
+}
+
 int main()
 {
     chip8 myChip8;
-    if (initializeChip8(&myChip8)) // Instantiate chip8 struct
-    {
-        printf("Failed to initialize chip8 cpu!\n");
-        exit(1);
-    }
-    else
-    {
-        printf("Successfully initialized chip8 cpu!\n");
-    }
+    display display;
+    checkFailure("initialize chip8 cpu", "initialized chip8 cpu", initializeChip8(&myChip8));
+    checkFailure("load game", "loaded game", loadGame(&myChip8, "test/test_opcode.ch8"));
+    checkFailure("setup display", "setup display", setupDisplay(&display));
 
-    if (loadGame(&myChip8, "test/test_opcode.ch8")) // Load test game
-    {
-        printf("Failed to load game!\n");
-        exit(1);
-    }
-    else
-    {
-        printf("Successfully loaded game!\n");
-    }
+    printf("Chip8 PC: %d\n", myChip8.pc);
+
     // for (int i = 0; i < 10; ++i)                // Emulate 10 cycles
     // {
     //     emulateCycle(&myChip8);
     //     myChip8.pc += 2;
     // }
 
-    // if (SDL_Init(SDL_INIT_VIDEO) != 0) // Try to initialize an SDL video component
-    // {
-    //     printf("error initializing SDL: %s\n", SDL_GetError());
-    //     exit(1);
-    // }
-    // else
-    // {
-    //     printf("Initialization successful\n");
-    // }
+    //
 
     // int baseWidth = 64;
     // int baseHeight = 32;
@@ -58,9 +88,6 @@ int main()
 
     // SDL_Init(SDL_INIT_EVERYTHING);
 
-    // display->window = SDL_CreateWindow(
-    //     "Chip 8 Emu", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-    //     windowWidth, windowHeight, SDL_WINDOW_SHOWN);
     // if (display_check_error("window", display->window))
     //     return -1;
 
