669ee008f357e566771f0b9686bb8c5a87c14726
Erik Haight <edhaight@ucsc.edu>
2019-07-28 14:17:05 -0700

    Cleanup

--- src/chip8.c
+++ src/chip8.c
@@ -1,71 +1,74 @@
 #include "chip8.h"
 
-unsigned short opcode = 0;
+static unsigned char chip8FontSet[80] =
+    {
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
 
-void cpuNULL(chip8 *myChip8)
+static void cpuNULL(chip8 *c8);
+static void cpuARITHMETIC(chip8 *c8);
+static void (*ArithmeticOpcode[])(chip8 *c8);
+static void (*execOpcode[])(chip8 *c8);
+
+static void cpuNULL(chip8 *c8)
 {
-    printf("%d\n", myChip8->pc);
+    printf("%d\n", c8->pc);
 }
 
-void (*ArithmeticOpcode[])(chip8 *myChip8) = {
+static void cpuARITHMETIC(chip8 *c8)
+{
+    ArithmeticOpcode[(c8->opcode & 0x000F)](c8);
+}
+
+static void (*ArithmeticOpcode[])(chip8 *c8) = {
     cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL,
-	cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL
-};
+    cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL};
 
-void cpuARITHMETIC(chip8 *myChip8){
-	ArithmeticOpcode[(myChip8->opcode&0x000F)](myChip8);
-}
+static void (*execOpcode[])(chip8 *c8) = {
+    cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL,
+    cpuARITHMETIC, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL,
+    cpuNULL};
 
-void (*execOpcode[])(chip8 *myChip8) = {
-    cpuNULL      , cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, 
-	cpuARITHMETIC, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL,
-	cpuNULL
-};
-
-void add()
+int initializeChip8(chip8 *c8)
 {
-    printf("OPCODE: 0x1A\n");
-}
+    c8->pc = 0x200; // Program counter starts at 0x200
+    c8->opcode = 0;
+    c8->I = 0;  // Reset index register
+    c8->sp = 0; // Reset stack pointer
 
-void sub()
-{
-    printf("OPCODE: 0x1D\n");
-}
-
-
-// ////////////////////////////////////////////////////////////////////////////////////////////
-
-// Chip8Table =
-//     {
-//         cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL,
-//         cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL, cpuNULL,
-//         cpuNULL};
-
-int initializeChip8(chip8 *myChip8)
-{
-    myChip8->pc = 0x200; // Program counter starts at 0x200
-    myChip8->opcode = 0;
-    myChip8->I = 0;      // Reset index register
-    myChip8->sp = 0;     // Reset stack pointer
-
-    if (memset(myChip8->gfx, 0, sizeof(myChip8->gfx)) == NULL) // Clear display
+    if (memset(c8->gfx, 0, sizeof(c8->gfx)) == NULL) // Clear display
         return -1;
-    if (memset(myChip8->stack, 0, sizeof(myChip8->stack)) == NULL) // Clear stack
+    if (memset(c8->stack, 0, sizeof(c8->stack)) == NULL) // Clear stack
         return -1;
-    if (memset(myChip8->V, 0, sizeof(myChip8->V)) == NULL) // Clear Registers
+    if (memset(c8->V, 0, sizeof(c8->V)) == NULL) // Clear Registers
         return -1;
-    if (memset(myChip8->memory, 0, sizeof(myChip8->memory)) == NULL) // Clear Memory
+    if (memset(c8->memory, 0, sizeof(c8->memory)) == NULL) // Clear Memory
         return -1;
-    if (memcpy(myChip8->memory, chip8FontSet, sizeof(chip8FontSet)) == NULL) // Load fontset
+    if (memcpy(c8->memory, chip8FontSet, sizeof(chip8FontSet)) == NULL) // Load fontset
         return -1;
 
-    myChip8->delayTimer = 0; // Reset delay Timer
-    myChip8->soundTimer = 0; // Reset sound Timer
+    c8->delayTimer = 0; // Reset delay Timer
+    c8->soundTimer = 0; // Reset sound Timer
 
     return 0;
 }
 
-int loadGame(chip8 *myChip8, char *filename)
+int loadGame(chip8 *c8, char *filename)
 {
     FILE *fptr;
     if ((fptr = fopen(filename, "rb")) == NULL) // Open in read binary mode
@@ -77,39 +80,33 @@ int loadGame(chip8 *myChip8, char *filename)
     long fsize = ftell(fptr); // Store # of bytes from beginning of file ( to end )
     fseek(fptr, 0, SEEK_SET); // Move file ptr to beginning of file
 
-    unsigned char *buffer = malloc(fsize + 1);      // Allocate space for the buffer
-    fread(buffer, 1, fsize, fptr);                  // Read from file into buffer for fsize bytes
-    memcpy(myChip8->memory + 0x200, buffer, fsize); // Copy buffer into chip memory
+    unsigned char *buffer = malloc(fsize + 1); // Allocate space for the buffer
+    fread(buffer, 1, fsize, fptr);             // Read from file into buffer for fsize bytes
+    memcpy(c8->memory + 0x200, buffer, fsize); // Copy buffer into chip memory
 
     free(buffer); // Free the memory allocated for the buffer
     fclose(fptr); // Close the file
     return 0;
 }
 
-void fetchOpcode(chip8 *myChip8)
-{
-    unsigned short pc = myChip8->pc;
-    opcode = myChip8->memory[pc] << 8 | myChip8->memory[pc + 1];
-    myChip8->opcode = opcode;
-    // printf("%d\n",opcode);
-    // Copy back pc to chip8 struct
-    myChip8->pc = pc;
-}
-void emulateCycle(chip8 *myChip8)
+void emulateCycle(chip8 *c8)
 {
     // Fetch Opcode
-    fetchOpcode(myChip8);
-    // decode and execute Opcode
-    execOpcode[(opcode&0xF000)>>12](myChip8);
-    
+    c8->opcode = c8->memory[c8->pc] << 8 | c8->memory[c8->pc + 1];
+    // decode Opcode
+    unsigned short decoded = (c8->opcode & 0xF000) >> 12;
+    // Execute Opcode
+    execOpcode[decoded](c8);
 
     // Update timers
-    if (myChip8->delayTimer > 0)
-        --myChip8->delayTimer;
-    if (myChip8->soundTimer > 0)
+    if (c8->delayTimer > 0)
     {
-        --myChip8->soundTimer;
-        if (myChip8->soundTimer == 0)
+        --c8->delayTimer;
+    }
+    if (c8->soundTimer > 0)
+    {
+        --c8->soundTimer;
+        if (c8->soundTimer == 0)
             printf("Alarm!\n");
     }
 }
